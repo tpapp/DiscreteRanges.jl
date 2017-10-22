b29ee6ba49d09307a81950d84049cc8930b39635
Tamas K. Papp <tkpapp@gmail.com>
2017-10-22 13:18:00 +0200

    Initial commit.

--- src/DiscreteRanges.jl
+++ src/DiscreteRanges.jl
@@ -1,5 +1,111 @@
 module DiscreteRanges
 
-# package code goes here
+using ArgCheck
+
+import Base:
+    show, isequal, ==, hash, convert,
+    in, isempty, issubset, union, intersect,
+    minimum, maximum, extrema,
+    length, size, IndexStyle, getindex
+
+export DiscreteRange, ..
+
+struct DiscreteRange{T} <: AbstractVector{T}
+    left::T
+    right::T
+    function DiscreteRange(left::T, right::T) where T
+        @argcheck isdiscrete(T) "Only discrete types are supported. Define `isdiscrete($T)`."
+        new{T}(left, right)
+    end
+end
+
+..(left, right) = DiscreteRange(left, right)
+
+isdiscrete(::Type) = false
+isdiscrete(::Type{<:Integer}) = true
+isdiscrete(::Type{Date}) = true
+
+discrete_gap(x::Integer, y::Integer) = x - y
+discrete_gap(x::Date, y::Date) = Dates.days(y - x)
+
+discrete_next(x) = discrete_next(x, 1)
+discrete_next(x::T, Δ) where {T <: Integer} = x + T(Δ)
+discrete_next(x::Date, Δ) = x + Dates.Day(Δ)
+
+DiscreteRange(x) = DiscreteRange(x, x) # single value
+
+isempty(D::DiscreteRange) = D.left > D.right
+
+function show(io::IO, D::DiscreteRange)
+    if isempty(D)
+        print(io, "empty $(typeof(D))")
+    else
+        print(io, D.left, "..", D.right)
+    end
+end
+
+show(io::IO, ::MIME"text/plain", D::DiscreteRange) =
+    show(io, D) # fallback to prevent AbstractArray printing
+
+in(x, D::DiscreteRange) = D.left ≤ x ≤ D.right
+
+in(A::DiscreteRange, B::DiscreteRange) = (B.left ≤ A.left) & (A.right ≥ B.right)
+
+isequal(A::DiscreteRange, B::DiscreteRange) =
+    (isequal(A.left, B.left) & isequal(A.right, B.right)) | (isempty(A) & isempty(B))
+
+==(A::DiscreteRange, B::DiscreteRange) =
+    (A.left == B.left) & (A.right == B.right) | (isempty(A) & isempty(B))
+
+const _DISCRETERANGE_HASH = UInt === UInt64 ? 0x3c467b76e5939730 : 0xff9d6b2d
+
+hash(A::DiscreteRange, h::UInt) = hash(A.left, hash(A.right, hash(_DISCRETERANGE_HASH, h)))
+
+function minimum(D::DiscreteRange)
+    @argcheck !isempty(D)
+    D.left
+end
+
+function maximum(D::DiscreteRange)
+    @argcheck !isempty(D)
+    D.right
+end
+
+function extrema(D::DiscreteRange)
+    @argcheck !isempty(D)
+    D.left, D.right
+end
+
+intersect(A::DiscreteRange, B::DiscreteRange) =
+    DiscreteRange(max(A.left, B.left), min(A.right, B.right))
+
+function union(A::DiscreteRange, B::DiscreteRange)
+    @argcheck(discrete_gap(max(A.left, B.left), min(A.right, B.right)) ≤ 1,
+              "Disjoint discrete ranges.")
+    DiscreteRange(min(A.left, B.left), max(A.right, B.right))
+end
+
+issubset(A::DiscreteRange, B::DiscreteRange) =
+    isempty(A) || (B.left ≤ A.left ≤ A.right ≤ B.right)
+
+length(D::DiscreteRange) = isempty(D) ? 0 : Int(discrete_gap(D.right, D.left)) + 1
+
+size(D::DiscreteRange) = (length(D),)
+
+IndexStyle(::Type{<:DiscreteRange}) = IndexLinear()
+
+function getindex(D::DiscreteRange, i)
+    i ≥ 1 || throw(BoundsError(D, i))
+    elt = discrete_next(D.left, i-1)
+    elt ≤ D.right || throw(BoundsError(D, i))
+    elt
+end
+
+start(D::DiscreteRange) = D.left
+next(D::DiscreteRange, state) = state, discrete_next(state)
+done(D::DiscreteRange, state) = state > D.right
+
+convert(::Type{R}, D::DiscreteRange) where {R <: StepRange} =
+    R(D.left, discrete_next(D.left) - D.left, D.right)
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,22 @@
 using DiscreteRanges
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+@testset "discrete range Int" begin
+    A = 1..5
+    @test A == DiscreteRange(1, 5)
+    @test isequal(A, DiscreteRange(1, 5))
+    @test A ≠ 3..7
+    @test 1 ∈ A
+    @test 5 ∈ A
+    @test 3 ∈ A
+    @test minimum(A) == 1
+    @test maximum(A) == 5
+    @test extrema(A) == (1,5)
+    @test A ∩ (2..7) == 2..5
+    @test A ∪ (6..10) == 1..10
+    @test_throws ArgumentError A ∪ (c7..10)
+    @test length(A) == 5
+    @test collect(A) == collect(1:5)
+    @test eltype(A) == Int
+    @test convert(StepRange, A) == 1:1:5
+end
